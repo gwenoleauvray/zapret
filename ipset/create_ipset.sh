d448909e41c9105a7b7e94c069575e18452cd4e6
bolvan <k@vodka.home.kg>
2019-06-19 12:26:06 +0300

    create_ipset.sh : saveram mode for 64 Mb RAM systems

--- ipset/create_ipset.sh
+++ ipset/create_ipset.sh
@@ -9,7 +9,71 @@ IP2NET="$EXEDIR/../ip2net/ip2net"
 
 . "$EXEDIR/def.sh"
 
+IPSET_CMD="$TMPDIR/ipset_cmd.txt"
+IPSET_SAVERAM_CHUNK_SIZE=20000
+
 [ "$1" = "no-update" ] && NO_UPDATE=1
+
+file_extract_lines()
+{
+ # $1 - filename
+ # $2 - from line (starting with 0)
+ # $3 - line count
+ # awk "{ err=1 } NR < $(($2+1)) { next } { print; err=0 } NR == $(($2+$3)) { exit err } END {exit err}" "$1"
+ awk "NR < $(($2+1)) { next } { print } NR == $(($2+$3)) { exit }" "$1"
+}
+ipset_restore_chunked()
+{
+ # $1 - filename
+ # $2 - chunk size
+ local pos lines
+ [ -f "$1" ] || return
+ lines=$(wc -l <"$1")
+ pos=$lines
+ while [ "$pos" -gt "0" ]; do
+    pos=$((pos-$2))
+    [ "$pos" -lt "0" ] && pos=0
+    file_extract_lines "$1" $pos $2 | ipset -! restore
+    sed -i "$(($pos+1)),$ d" "$1"
+ done
+}
+
+
+ipset_get_script()
+{
+ # $1 - filename
+ # $2 - ipset name
+ # $3 - exclude file
+ # $4 - "6" = ipv6
+ if [ -x "$IP2NET" ] && [ "$4" != "6" ]; then
+  if [ -f "$3" ] ; then
+   zzcat "$1" | grep -vxFf "$3" | "$IP2NET" | sed -nre "s/^.+$/add $2 &/p"
+  else
+   zzcat "$1" | "$IP2NET" | sed -nre "s/^.+$/add $2 &/p"
+  fi
+ else
+  if [ -f "$3" ] ; then
+   zzcat "$1" | grep -vxFf "$3" | sort -u | sed -nre "s/^.+$/add $2 &/p"
+  else
+   zzcat "$1" | sort -u | sed -nre "s/^.+$/add $2 &/p"
+  fi
+ fi
+}
+
+ipset_restore()
+{
+  # $1 - filename
+  # $2 - ipset name
+  # $3 - exclude file
+  # $4 - "6" = ipv6
+  if [ "$SAVERAM" = "1" ]; then
+   ipset_get_script "$1" "$2" "$3" "$4" >"$IPSET_CMD"
+   ipset_restore_chunked "$IPSET_CMD" $IPSET_SAVERAM_CHUNK_SIZE
+   rm -f "$IPSET_CMD"
+  else
+   ipset_get_script "$1" "$2" "$3" "$4" | ipset -! restore
+  fi
+}
 
 create_ipset()
 {
@@ -26,21 +90,12 @@ ipset flush $2
 for f in "$3" "$4"
 do
  zzexist "$f" && {
-  if [ -x "$IP2NET" ]; then
-   echo Adding to ipset $2 \($IPSTYPE , ip2net\) : $f
-   if [ -f "$5" ] ; then
-    zzcat "$f" | grep -vxFf "$5" | "$IP2NET" | sed -nre "s/^.+$/add $2 &/p" | ipset -! restore
-   else
-    zzcat "$f" | "$IP2NET" | sed -nre "s/^.+$/add $2 &/p" | ipset -! restore
-   fi
-  else
-   echo Adding to ipset $2 \($IPSTYPE\) : $f
-   if [ -f "$5" ] ; then
-    zzcat "$f" | grep -vxFf "$5" | sort -u | sed -nre "s/^.+$/add $2 &/p" | ipset -! restore
-   else
-    zzcat "$f" | sort -u | sed -nre "s/^.+$/add $2 &/p" | ipset -! restore
-   fi
-  fi
+  local T="Adding to ipset $2 ($IPSTYPE"
+  [ -x "$IP2NET" ] && T="$T, ip2net"
+  [ "$SAVERAM" = "1" ] && T="$T, saveram"
+  T="$T) : $f"
+  echo $T
+  ipset_restore "$f" "$2" "$5" 4
  }
 done
 return 0
@@ -56,16 +111,22 @@ ipset flush $2
 for f in "$3" "$4"
 do
  zzexist "$f" && {
-   echo Adding to ipset $2 \($IPSTYPE\) : $f
-   if [ -f "$5" ] ; then
-    zzcat "$f" | grep -vxFf "$5" | sort -u | sed -nre "s/^.+$/add $2 &/p" | ipset -! restore
-   else
-    zzcat "$f" | sort -u | sed -nre "s/^.+$/add $2 &/p" | ipset -! restore
-   fi
+  local T="Adding to ipset $2 ($IPSTYPE"
+  [ "$SAVERAM" = "1" ] && T="$T, saveram"
+  T="$T) : $f"
+  echo $T
+  ipset_restore "$f" "$2" "$5" 6
  }
 done
 return 0
 }
+
+# ipset seem to buffer the whole script to memory
+# on low RAM system this can cause oom errors
+# in SAVERAM mode we feed script lines in portions starting from the end, while truncating source file to free /tmp space
+RAMSIZE=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+SAVERAM=0
+[ "$RAMSIZE" -lt "120000" ] && SAVERAM=1
 
 [ "$DISABLE_IPV4" != "1" ] && {
   create_ipset hash:ip $ZIPSET "$ZIPLIST" "$ZIPLIST_USER" "$ZIPLIST_EXCLUDE"
